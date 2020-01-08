f7b1a356d83ada81e0d73596b86630e51bb9684e
Pasindu Vimansa Kulasooriya <pasinduvimansa96@gmail.com>
2020-01-08 21:33:57 +0530

    Added css and js libraries

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,80 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header"> {{ isset($url) ? ucwords($url) : ""}}{{ __('Login') }}</div>
+    <title>RCSM v1.0.0</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('dist/img/favicon.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('dist/img/favicon.png')}}">
+    <!-- Fonts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-                <div class="card-body">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-image: url('https://i.pinimg.com/originals/41/aa/09/41aa09f7665923b7baa82997cdb49d67.gif');
+            background-size: cover;
+            background-position: center;
+            color: whitesmoke;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        a{
+            text-decoration: none;
+        }
+
+        .links > a {
+            color: whitesmoke;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .card { background-color: rgba(0, 0, 0, 0.84); }
+        .card-header, .card-footer { opacity: 1;color: whitesmoke}
+
+        label{
+            color: whitesmoke;
+        }
+    </style>
+</head>
+<body >
+<div class="flex-center position-ref full-height">
+
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card ">
+                    <div class="card-header">Login</div>
+                    <div class="card-body">
                     @isset($url)
                     <form method="POST"action='{{ url("login/$url") }}' aria-label="{{ __('Login') }}">
                         @else
@@ -69,9 +136,15 @@
                             </div>
                         </div>
                     </form>
+                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+
 </div>
-@endsection
+
+</body>
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,20 +4,47 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>PUSL 2002</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('dist/img/favicon.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('dist/img/favicon.png')}}">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="stylesheet" href="{{asset('bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('bower_components/font-awesome/css/font-awesome.min.css')}}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="{{asset('bower_components/Ionicons/css/ionicons.min.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('dist/css/AdminLTE.min.css')}}">
+    <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+          page. However, you can choose any other skin. Make sure you
+          apply the skin class to the body tag so the changes take effect. -->
+    <link rel="stylesheet" href="{{asset('dist/css/skins/skin-blue.min.css')}}">
+    <!-- jvectormap -->
+    <link rel="stylesheet" href="{{asset('bower_components/jvectormap/jquery-jvectormap.css')}}">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{asset('bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('plugins/pace/pace.min.css')}}">
+
+    <link rel="stylesheet" href="{{asset('plugins/iCheck/all.css')}}">
+
+    <link rel="stylesheet" href="{{asset('bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
+
+    <!-- daterange picker -->
+    <link rel="stylesheet" href="{{asset('bower_components/bootstrap-daterangepicker/daterangepicker.css')}}">
+
+    <link rel="stylesheet" href="{{asset('bower_components/select2/dist/css/select2.min.css')}}">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
     <div id="app">
@@ -76,5 +103,47 @@
             @yield('content')
         </main>
     </div>
+
+    <script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{asset('bower_components/jquery-ui/jquery-ui.min.js')}}"></script>
+    <script src="{{asset('bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
+
+    <!-- Select2 -->
+    <script src="{{asset('bower_components/select2/dist/js/select2.full.min.js')}}"></script>
+    <!-- date-range-picker -->
+    <script src="{{asset('bower_components/moment/min/moment.min.js')}}"></script>
+    <script src="{{asset('bower_components/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
+    <!-- Bootstrap 3.3.7 -->
+    <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{asset('dist/js/adminlte.min.js')}}"></script>
+
+    <!-- FastClick -->
+    <script src="{{asset('bower_components/fastclick/lib/fastclick.js')}}"></script>
+    <!-- Sparkline -->
+    <script src="{{asset('bower_components/jquery-sparkline/dist/jquery.sparkline.min.js')}}"></script>
+    <!-- SlimScroll -->
+    <script src="{{asset('bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+    <!-- ChartJS -->
+    <!-- jvectormap  -->
+    <script src="{{asset('plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
+    <script src="{{asset('plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+    <!--Data tables -->
+    <script src="{{asset('bower_components/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
+
+
+    <script src="{{asset('bower_components/PACE/pace.min.js')}}"></script>
+
+    <script src="{{asset('plugins/iCheck/icheck.min.js')}}"></script>
+
+
+    <script src="{{asset('plugins/input-mask/jquery.inputmask.js')}}"></script>
+    <script src="{{asset('plugins/input-mask/jquery.inputmask.date.extensions.js')}}"></script>
+    <script src="{{asset('plugins/input-mask/jquery.inputmask.extensions.js')}}"></script>
+
+    <script src="{{asset('bower_components/chart.js/Chart.js')}}"></script>
+
+    <script src="https://code.highcharts.com/highcharts.js"></script>
 </body>
 </html>
